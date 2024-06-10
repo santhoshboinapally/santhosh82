27b893ca71f9cc1f327c3b96bf32f544c52df839
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2024-06-10 12:13:19 +0200

    React files added

---
+++ src/components/navbar.js
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./navbar.css";
+import logo from '../images/logo.png';
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<img className="logo" src= {logo} alt="logo" />
+ 			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
