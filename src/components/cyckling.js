616e25df4142eb52e42a8029edc087796a28ef6e
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2024-06-27 09:31:27 +0200

    marquee updated

--- src/components/cyckling.js
+++ src/components/cyckling.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import cyckling from "../components/images/bike-bicycle.gif"
+import './cyckling.css'
 
 export const Cyckling = () => {
   return (
-    <div style={{backgroundColor:'var(--mainColor)'}}>
-        <marquee  direction="right">
-        <img src = {cyckling} alt= "cyckle" style={{height:"80px",width:"80px"}}/>
-       </marquee> 
+    <div className='marquee' style={{backgroundColor:'var(--mainColor)'}}>
+       <img src = {cyckling} alt= "cyckle" style={{height:"80px",width:"80px"}}/>
     </div>
   )
 }
